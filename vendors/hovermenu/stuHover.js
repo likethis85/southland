5dfbdabe1ce16201b46206a813f9530d13dfa624
eon <eon.hong@gmail.com>
2013-05-29 23:22:06 +0800

    hovermenu

--- vendors/hovermenu/stuHover.js
+++ vendors/hovermenu/stuHover.js
@@ -1,9 +1,28 @@
-$.fn.HoverMenu = function(hm,options) {
+$.fn.HoverMenu = function(options) {
     var settings = $.extend({'lazy':false}, options);
     
-    if(settings.lazy){
-        
-    } else {
-        
+    var m = $('<div style="background:transparent;position:absolute;"><div style="width:16px;height:16px;float:left;background:transparent;"></div><div style="width:7px;height:16px;float:left;background:transparent;"></div></div>');
+    m.hover(
+        function(){
+            $(this).css({'border':'1px solid #0080FF'});
+            //$(this).find('div:last').css('background-color','#0080FF');
+            $(this).click(function(){
+                $(this).find('div:last').slideDown();
+            });
+            $(this).find('div:last').css('background-color','#0080FF');
+        },
+        function(){
+            $(this).css({'border':'none'});
+            $(this).find('div:last').css('background-color','transparent');
+            $(this).find('div:last').slideUp();
+        }
+    );
+    
+    var menus = '<div style="display:none;"><ul>';
+    for(var i=0; i<settings.item.length; i++){
+        menus += '<li><img src="'+settings.item[i].icon+'" width=16 height=16></li>';
     }
+    menus += '</ul></div>';
+    m.append($(menus));
+    this.append(m);
 }
