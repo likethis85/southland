334a929101d5499e0070fba20c9bd1558aaf10f0
eon hong <eon.hong@gmail.com>
2013-05-30 04:26:13 -0700

    hovermenu

--- vendors/hovermenu/stuHover.js
+++ vendors/hovermenu/stuHover.js
@@ -1,14 +1,14 @@
 $.fn.HoverMenu = function(options) {
     var settings = $.extend({'lazy':false}, options);
-    
+    var _e = this[0];
     var m = $('<div style="background:transparent;position:absolute;">'+
                 '<div style="width:23px;height:16px;background:transparent;"><div id="hm_icon"  style="width:16px;height:16px;float:left;background:transparent;"></div>' +
                 '<div id="hm_arrow" style="width:7px;height:16px;float:left;background:transparent;"></div></div>' +
               '</div>');
     m.hover(
         function(){
-            $(this).find('div:first').css({'border':'1px solid #0080FF'});
-            $(this).find('#hm_arrow').css('background-color','#0080FF');
+            $(this).find('div:first').css({'border':'1px solid #6E88B7'});
+            $(this).find('#hm_arrow').css('background-color','#6E88B7');
             $(this).click(function(){
                 $(this).find('#hm_menu').slideDown('fast');
             });
@@ -20,9 +20,9 @@ $.fn.HoverMenu = function(options) {
         }
     );
     
-    var menus = '<div id="hm_menu" style="z-index:100;border:1px solid #0080FF;display:none;clear:both;background:white;"><ul>';
+    var menus = '<div id="hm_menu" style="border:1px solid #0080FF;display:none;background:white;position:relative;top:0px;z-index:100;"><ul>';
     for(var i=0; i<settings.item.length; i++){
-        menus += '<li><img style="margin-right:6px;" src="'+settings.item[i].icon+'" width=16 height=16>';
+        menus += '<li id="'+i+'"><img style="margin-right:6px;" src="'+settings.item[i].icon+'" width=16 height=16>';
         if(typeof settings.item[i].caption=='string')
             menus += settings.item[i].caption
         menus += '</li>';
@@ -30,9 +30,11 @@ $.fn.HoverMenu = function(options) {
     menus += '</ul></div>';
     menus = $(menus);
     menus.find('li').css({'padding':'2px'}).hover(
-        function() { $(this).css('background','#0080FF');},
+        function() { $(this).css('background','#6E88B7');},
         function() { $(this).css('background','white');}
-    );
+    ).click(function(){
+        settings.item[this.id].callback(_e);
+    });
     
     m.append(menus);
     this.append(m);
