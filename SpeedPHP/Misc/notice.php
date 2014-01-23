2982cb0a4f15af8c1f7f64d082378f4e717cd0fb
eon hong <eon.hong@gmail.com>
2014-01-23 18:19:45 +0800

    debug/release for notice

--- SpeedPHP/Misc/notice.php
+++ SpeedPHP/Misc/notice.php
@@ -1,3 +1,7 @@
+<?php if ($GLOBALS['G_SP']['mode']!='debug'){ ?>
+<html lang="en"><head><meta http-equiv="Content-Type" content="text/html; charset=utf-8"><meta name="robots" content="noindex"><title>Not found.</title></head><body><table height="100%" width="100%"><tbody><tr><td align="center" valign="middle"><div id="container"><h1 id="l10n_title">Not found.</h1><div class="sorry"><p id="l10n_sorry_1">We couldn't find the page you were looking for.</p></div></div></td></tr></tbody></table></body></html> <style type="text/css">body{height:100%;font-family:Arial,'Helvetica Neue',Helvetica,sans-serif;text-align:center;border:0;background:#21586C url(http://www.yupage.com/img/bg.png)repeat center;margin:0;padding:0}#container{width:400px}h1{color:#FFF;font-size:30px;font-weight:700;text-shadow:0 1px 4px rgba(0,0,0,0.68);letter-spacing:-1px;margin:0}.sorry{color:#D6D6D6;font-size:18px;line-height:24px;text-shadow:0 1px 1px rgba(255,255,255,0.33)}html{height:100%;-webkit-font-smoothing:antialiased;border:0;overflow-y:scroll;margin:0;padding:0}</style>
+
+<?php } else { ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
@@ -28,7 +32,7 @@ $souceline = getsource($trace["file"], $trace["line"]);if( $souceline ){
 <?php 
 foreach( $souceline as $singleline )echo $singleline;?>
 </div>
-<?php }}}?>
+<?php }}}}?>
 </div></div><div style="clear:both;"></body></html>
 <?php
 function getsource($file, $line){
